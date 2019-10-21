8c78e6c39b9b91476cba878fe50537aeabe3bc66
Hannes Alarotu <hannes.alarotu@codento.com>
2019-10-21 16:22:42 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+function Birds({birds}) {
+  birds = birds.sort(function(a, b) {
+    if  (a.nameFin > b.nameFin) {
+      return 1
+    }
+    return -1
+  })
+
+  return (
+    <div>
+      <h1>Suomessa havaitut lintulajit</h1>
+      {birds ? birds.map((bird, i) => 
+        <Bird bird={bird} key={i} />) :
+        <div>Ei lintuja!</div>}
+    </div>
+  )
+}
+
+function Bird({bird}) {
+  return (
+    <div>{bird.nameFin}</div>
+  )
+}
+
 function App() {
+  const [birds, setBirds] = useState([])
+
+  useEffect(() => {getBirds()}, [])
+
+  const getBirds = () => {
+      fetch(`http://localhost:8080/bird`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json() 
+        } else {
+          throw new Error(res)
+        }
+      })
+      .then(birds => setBirds(birds))
+      .catch(error => console.log(error))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Birds birds={birds} />
     </div>
-  );
+  )
 }
+
 
 export default App;
